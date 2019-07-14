40855850536a34cab4756faf92160b09fe1cdb66
Alexander Schwartzberg <aeksco@gmail.com>
2019-07-13 22:35:52 -0400

    Cleaned up pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,8 @@ import { Container, Header, Image } from "semantic-ui-react";
 const Index = () => {
   return (
     <Container text style={{ marginTop: "7em" }}>
-      <Header as="h1">Semantic UI React Fixed Template</Header>
-      <p>This is a basic fixed menu template using fixed size containers.</p>
-      <p>
-        A text container is used for the main container, which is useful for
-        single column layouts.
-      </p>
+      <Header as="h1">Nuxt Netlify Lambda Starter</Header>
+      <p>SEO-friendly website starter backed by Netlify lambda functions</p>
       <Image
         src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png"
         style={{ marginTop: "2em" }}
