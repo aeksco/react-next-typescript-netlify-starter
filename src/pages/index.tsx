a833bbf7941129bd968ab5f4c15c54f02e4ae272
Alexander Schwartzberg <aeksco@gmail.com>
2020-11-18 00:32:23 -0500

    Added Hello component to pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
+import { Hello } from "../components/Hello";
 
 export default function () {
   return (
     <div className="row">
       <h1>Next.js + TypeScript + Tailwind CSS Starter</h1>
       <p>SEO-friendly website starter backed by Netlify lambda functions</p>
-      <button className="btn btn-blue">Hello!</button>
+      <Hello />
     </div>
   );
 }
