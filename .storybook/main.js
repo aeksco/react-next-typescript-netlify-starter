33e528729ca49e817f9e56acdb0e2ac2c6e3f278
Alexander Schwartzberg <aeksco@gmail.com>
2020-11-17 23:46:51 -0500

    Added .storybook files - manager.js, manager-head.html, preview-head.html, theme.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,35 @@
 module.exports = {
-  stories: ["../src/**/*.stories.@(ts|tsx|js|jsx|mdx)"],
+  stories: ["../src/**/*.stories.@(ts|tsx|js|jsx)", "../docs/**/*.stories.mdx"],
   addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          // Filter out the default .css rule.
+          ...config.module.rules.filter((rule) => /\.css$/ !== rule.test),
+          // Add our own css rule which in turn will read the postcss.config.js from project root.
+          {
+            test: /\.css1$/,
+            exclude: [/\.module\.css$/, /@storybook/],
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
+                options: { importLoaders: 1, sourceMap: false },
+              },
+              {
+                loader: "postcss-loader",
+                options: {
+                  ident: "postcss",
+                  sourceMap: false,
+                },
+              },
+            ],
+          },
+        ],
+      },
+    };
+  },
 };
---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+// import LogoSvg from "./assets/logo.svg";
+
+export default create({
+  base: "light", // or "dark"
+  brandTitle: "Next.js TypeScript Starter",
+  brandUrl: "https://github.com/aeksco/react-next-typescript-netlify-starter",
+  // brandImage: LogoSvg
+});
