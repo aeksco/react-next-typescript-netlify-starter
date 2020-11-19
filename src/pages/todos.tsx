030fbb6e5169b69b50593eb2b300352a8e097d29
Alexander Schwartzberg <aeksco@gmail.com>
2020-11-18 22:10:07 -0500

    Added pages/todos.tsx example

---
+++ src/pages/todos.tsx
@@ -0,0 +1,227 @@
+import * as React from "react";
+
+// // // //
+
+// function Debug(props) {
+//     return <pre>{JSON.stringify(props, null, 4)}</pre>;
+// }
+
+// // // //
+
+type Item = {
+    id: number;
+    name: string;
+    done: boolean;
+};
+
+function ItemForm(props: {
+    name: string;
+    onSubmit: (submittedName: string) => void;
+}) {
+    const [name, setName] = React.useState<string>(props.name);
+    return (
+        <form
+            className="flex flex-row w-full mt-2 border-t border-b border-gray-400 py-4"
+            onSubmit={e => {
+                e.preventDefault();
+
+                props.onSubmit(name);
+                setName("");
+            }}
+        >
+            <input
+                required
+                className="border border-gray-500 rounded w-full py-2 px-2"
+                placeholder="New Item"
+                value={name}
+                onChange={e => {
+                    setName(e.currentTarget.value);
+                }}
+            />
+            <button
+                type="submit"
+                className="btn bg-green-400 hover:bg-green-600 text-white ml-4"
+            >
+                &#10004;
+            </button>
+        </form>
+    );
+}
+
+// // // //
+
+function ListConrols(props: {
+    items: Item[];
+    onChange: (updatedItems: Item[]) => void;
+}) {
+    const { items, onChange } = props;
+    return (
+        <div className="flex flex-row w-full justify-between mt-2 border-b border-gray-400 py-4">
+            <button
+                onClick={() => {
+                    onChange(
+                        items.map(i => {
+                            return {
+                                ...i,
+                                done: true,
+                            };
+                        }),
+                    );
+                }}
+                className="flex flex-grow justify-center btn bg-green-400 hover:bg-green-600 text-white mx-2"
+            >
+                Check All
+            </button>
+            <button
+                onClick={() => {
+                    onChange(
+                        items.map(i => {
+                            return {
+                                ...i,
+                                done: false,
+                            };
+                        }),
+                    );
+                }}
+                className="flex flex-grow justify-center btn bg-gray-400 hover:bg-gray-600 text-white mx-2"
+            >
+                Uncheck All
+            </button>
+            <button
+                onClick={() => {
+                    onChange(items.filter(i => !i.done));
+                }}
+                className="flex flex-grow justify-center btn bg-red-400 hover:bg-red-600 text-white mx-2"
+            >
+                Remove All Completed
+            </button>
+            <button
+                onClick={() => {
+                    onChange([]);
+                }}
+                className="flex flex-grow justify-center btn bg-red-400 hover:bg-red-600 text-white mx-2"
+            >
+                Remove All
+            </button>
+        </div>
+    );
+}
+
+// // // //
+
+/**
+ * TodoList
+ * Renders a component for managing an Array<Item>
+ * @param props.items - the array of `Item` instances being edited here
+ */
+function TodoList(props: { items: Item[] }) {
+    const [items, setItems] = React.useState<Item[]>(props.items);
+
+    function addItem(newItemName: string) {
+        setItems([
+            ...items,
+            {
+                id: Math.random(),
+                name: newItemName,
+                done: false,
+            },
+        ]);
+    }
+
+    if (items.length === 0) {
+        return (
+            <div className="flex flex-col w-full pt-4 items-center">
+                <ItemForm
+                    name=""
+                    onSubmit={updatedName => {
+                        addItem(updatedName);
+                    }}
+                />
+                <p className="text-lg text-gray-900 mt-4">
+                    There's nothing left to do today :)
+                </p>
+                <p className="text-sm text-gray-500">
+                    Go grab yourself a nice cold beer
+                </p>
+            </div>
+        );
+    }
+
+    return (
+        <div className="flex flex-col w-full">
+            <ItemForm
+                name=""
+                onSubmit={updatedName => {
+                    addItem(updatedName);
+                }}
+            />
+            <ListConrols items={items} onChange={setItems} />
+            {items.map(item => {
+                return (
+                    <li
+                        className="flex py-4 flex-row w-full justify-between items-center border-2 border-t-0 border-l-0 border-r-0 border-gray-400"
+                        key={item.id}
+                        onClick={() => {
+                            console.log(item.id);
+                        }}
+                    >
+                        <div className="flex items-center">
+                            <button
+                                onClick={() => {
+                                    setItems(
+                                        items.map(i => {
+                                            if (i.id === item.id) {
+                                                return {
+                                                    ...item,
+                                                    done: !item.done,
+                                                };
+                                            }
+                                            return i;
+                                        }),
+                                    );
+                                }}
+                            >
+                                {item.done && (
+                                    <p className="text-green-400 text-4xl">
+                                        &#9745;
+                                    </p>
+                                )}
+                                {!item.done && (
+                                    <p className="text-gray-400 text-4xl">
+                                        &#9745;
+                                    </p>
+                                )}
+                            </button>
+                            <p className="ml-2">{item.name}</p>
+                        </div>
+                        <button
+                            onClick={() => {
+                                setItems(items.filter(i => i.id !== item.id));
+                            }}
+                            className="btn bg-red-600 text-white hover:bg-red-700	"
+                        >
+                            Remove
+                        </button>
+                    </li>
+                );
+            })}
+        </div>
+    );
+}
+
+// // // //
+
+export default function() {
+    return (
+        <div className="px-10 py-10">
+            <h1 className="text-xl">Todo List</h1>
+            <TodoList
+                items={[
+                    { id: 1, name: "Brush teeth", done: false },
+                    { id: 2, name: "Eat", done: true },
+                    { id: 3, name: "Sleep", done: false },
+                ]}
+            />
+        </div>
+    );
+}
